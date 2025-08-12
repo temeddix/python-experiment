40b86a9f49b9d95e6f7831eb5c84a09dc0c65392
Danny Kim <temeddix@gmail.com>
2025-08-12 23:24:09 +0900

    Properly shutdown the executor

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import asyncio
-from asyncio import Future
+from asyncio import Future, AbstractEventLoop
 from concurrent.futures import InterpreterPoolExecutor
 from time import time
 from helper import square
 
 
-async def test_speed(max_workers: int):
+async def test_speed(
+    max_workers: int,
+    pool: InterpreterPoolExecutor,
+    loop: AbstractEventLoop,
+):
     start_time = time()
-    pool = InterpreterPoolExecutor(max_workers)
-    loop = asyncio.get_running_loop()
     futures: list[Future[int]] = []
     for num in range(100):
         future = loop.run_in_executor(pool, square, num)
@@ -24,8 +26,10 @@ async def test_speed(max_workers: int):
 
 
 async def main():
+    loop = asyncio.get_running_loop()
     for max_workers in (1, 2, 4, 8, 16, 32, 64):
-        await test_speed(max_workers)
+        with InterpreterPoolExecutor(max_workers) as pool:
+            await test_speed(max_workers, pool, loop)
 
 
 if __name__ == "__main__":
